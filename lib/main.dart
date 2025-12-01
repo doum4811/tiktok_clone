cbf19bffb2d5936793a6e1ace2b973488246c54e
doum <notivior@gmail.com>
2025-12-01 20:35:18 +0900

    feat: #3.1 Constants

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
 
 void main() {
   runApp(const TikTokApp());
@@ -12,7 +14,12 @@ class TikTokApp extends StatelessWidget {
     return MaterialApp(
       title: 'TikTok Clone',
       theme: ThemeData(primaryColor: Color(0xFFE9435A)),
-      home: Container(),
+      home: Padding(
+        padding: const EdgeInsets.all(Sizes.size14),
+        child: Container(
+          child: Row(children: [Text('hello'), Gaps.h20, Text('hello')]),
+        ),
+      ),
     );
   }
 }
