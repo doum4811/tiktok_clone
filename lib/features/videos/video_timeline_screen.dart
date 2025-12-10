fdface820ac09c7767c44417708ebf6efdea4bc2
doum <notivior@gmail.com>
2025-12-10 14:49:56 +0900

    feat: #7.1 Infinite Scrolling

---
+++ lib/features/videos/video_timeline_screen.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class VideoTimelineScreen extends StatefulWidget {
+  const VideoTimelineScreen({super.key});
+
+  @override
+  State<VideoTimelineScreen> createState() => _VideoTimelineScreenState();
+}
+
+class _VideoTimelineScreenState extends State<VideoTimelineScreen> {
+  int _itemCount = 4;
+
+  List<Color> colors = [Colors.blue, Colors.red, Colors.yellow, Colors.teal];
+
+  void _onPageChanged(int page) {
+    // print(page);
+    if (page == _itemCount - 1) {
+      _itemCount = _itemCount + 4;
+      colors.addAll([Colors.blue, Colors.red, Colors.yellow, Colors.teal]);
+      setState(() {});
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView.builder(
+      scrollDirection: Axis.vertical,
+      // pageSnapping: false,
+      // children: [
+      //   Container(color: Colors.blue),
+      //   Container(color: Colors.teal),
+      //   Container(color: Colors.yellow),
+
+      //   Container(color: Colors.pink),
+      // ],
+      onPageChanged: _onPageChanged,
+      itemCount: _itemCount,
+      itemBuilder: (context, index) => Container(
+        color: colors[index],
+        child: Center(
+          child: Text("Screen $index", style: TextStyle(fontSize: 68)),
+        ),
+      ),
+    );
+  }
+}
