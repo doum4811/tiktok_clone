283147f3b44bb59e9d05934740be6518a62444c1
doum <notivior@gmail.com>
2025-12-12 16:59:38 +0900

    feat: #9.2 TabBar

--- lib/features/discover/discover_screen.dart
+++ lib/features/discover/discover_screen.dart
@@ -1,11 +1,41 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+final tabs = ["Top", "Users", "Video", "Sounds", "LIVE", "Shopping", "Brands"];
 
 class DiscoverScreen extends StatelessWidget {
   const DiscoverScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("Discover")));
+    return DefaultTabController(
+      length: tabs.length,
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 1,
+          title: Text("Discover"),
+          bottom: TabBar(
+            splashFactory: NoSplash.splashFactory,
+            padding: EdgeInsets.symmetric(horizontal: Sizes.size16),
+            isScrollable: true,
+            labelStyle: TextStyle(
+              fontWeight: FontWeight.w600,
+              fontSize: Sizes.size16,
+            ),
+            indicatorColor: Colors.black,
+            labelColor: Colors.black,
+            unselectedLabelColor: Colors.grey.shade500,
+            tabs: [for (var tab in tabs) Tab(text: tab)],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            for (var tab in tabs)
+              Center(child: Text(tab, style: TextStyle(fontSize: 28))),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/features/main_navigation/main_navigation_screen.dart
+++ lib/features/main_navigation/main_navigation_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/discover/discover_screen.dart';
 import 'package:tiktok_clone/features/main_navigation/widgets/nav_tab.dart';
 import 'package:tiktok_clone/features/main_navigation/widgets/post_video_button.dart';
 import 'package:tiktok_clone/features/videos/video_timeline_screen.dart';
@@ -15,7 +16,7 @@ class MainNavigationScreen extends StatefulWidget {
 }
 
 class _MainNavigationScreenState extends State<MainNavigationScreen> {
-  int _selectedIndex = 0;
+  int _selectedIndex = 1; //0;
 
   void _onTap(int index) {
     setState(() {
@@ -42,7 +43,7 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
       Stack(
         children: [
           Offstage(offstage: _selectedIndex != 0, child: VideoTimelineScreen()),
-          Offstage(offstage: _selectedIndex != 1, child: Container()),
+          Offstage(offstage: _selectedIndex != 1, child: DiscoverScreen()),
 
           Offstage(offstage: _selectedIndex != 3, child: Container()),
           Offstage(offstage: _selectedIndex != 4, child: Container()),
