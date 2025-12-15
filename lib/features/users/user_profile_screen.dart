ba666dc57cb265c72e0fefb5e290bb5e7950a3ed
doum <notivior@gmail.com>
2025-12-15 17:44:37 +0900

    feat: #12.1 CustomScrollView

--- lib/features/users/user_profile_screen.dart
+++ lib/features/users/user_profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class UserProfileScreen extends StatefulWidget {
   const UserProfileScreen({super.key});
@@ -10,6 +11,30 @@ class UserProfileScreen extends StatefulWidget {
 class _UserProfileScreenState extends State<UserProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return CustomScrollView();
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          floating: true,
+          stretch: true,
+          pinned: true,
+          backgroundColor: Colors.teal,
+          // title: Text("Hello!"),
+          collapsedHeight: 80,
+          expandedHeight: 200,
+          flexibleSpace: FlexibleSpaceBar(
+            stretchModes: [
+              StretchMode.blurBackground,
+              // StretchMode.fadeTitle,
+              StretchMode.zoomBackground,
+            ],
+            background: Image.asset(
+              "assets/images/IMG_3363.PNG",
+              fit: BoxFit.cover,
+            ),
+            title: Text("Hello!"),
+          ),
+        ),
+      ],
+    );
   }
 }
