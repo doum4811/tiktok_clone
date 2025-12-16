b4c6fe15d3d6e7afdf77a5d7589a774b88d64415
doum <notivior@gmail.com>
2025-12-16 21:22:42 +0900

    feat: #13.2 showDateRangePicker

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -11,13 +11,60 @@ class SettingsScreen extends StatelessWidget {
       body: ListView(
         children: [
           ListTile(
-            onTap: () => showAboutDialog(
-              context: context,
-              applicationVersion: "1.0",
-              applicationLegalese: "All right reseverd. Please dont copy me",
-            ),
-            title: Text("About", style: TextStyle(fontWeight: FontWeight.w600)),
-            subtitle: Text("About this app......"),
+            onTap: () async {
+              final date = await showDatePicker(
+                context: context,
+                initialDate: DateTime.now(),
+                firstDate: DateTime(1980),
+                lastDate: DateTime(2030),
+              );
+              print(date);
+
+              final time = await showTimePicker(
+                context: context,
+                initialTime: TimeOfDay.now(),
+              );
+              print(time);
+              final booking = await showDateRangePicker(
+                context: context,
+                firstDate: DateTime(1980),
+                lastDate: DateTime(2030),
+
+                // // 문제는 해결되었던 것 같아보임
+                // // 따라해보았으나 다르게 나옴 확인 필요
+                // builder: (context, child) {
+                //   return Theme(
+                //     data: ThemeData(
+                //       appBarTheme: AppBarTheme(
+                //         foregroundColor: Colors.white,
+                //         backgroundColor: Colors.black,
+                //       ),
+                //     ),
+                //     child: child!,
+                //   );
+
+                // 이건 변화를 볼 수 있음
+                builder: (context, child) {
+                  return Theme(
+                    data: Theme.of(context).copyWith(
+                      colorScheme: ColorScheme.light(
+                        primary: Colors.amberAccent,
+                        onPrimary: Colors.redAccent,
+                        onSurface: Colors.blueAccent,
+                      ),
+                      textButtonTheme: TextButtonThemeData(
+                        style: TextButton.styleFrom(
+                          foregroundColor: Colors.red,
+                        ),
+                      ),
+                    ),
+                    child: child!,
+                  );
+                },
+              );
+              print(booking);
+            },
+            title: Text("What is your birthday?"),
           ),
           AboutListTile(),
         ],
