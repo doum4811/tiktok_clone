53a78677c57fa5dd7cfb0f4ec9ab81a7aa70fb10
doum <notivior@gmail.com>
2025-12-16 20:47:55 +0900

    feat: #13.1 AboutListTile

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -8,38 +8,20 @@ class SettingsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Settings')),
-      body: Column(
+      body: ListView(
         children: [
-          // CupertinoActivityIndicator(
-          //   radius: 40,
-          //   // animating: false, // 움직이지 않음
-          // ),
-          // CircularProgressIndicator(),
-          CircularProgressIndicator.adaptive(),
+          ListTile(
+            onTap: () => showAboutDialog(
+              context: context,
+              applicationVersion: "1.0",
+              applicationLegalese: "All right reseverd. Please dont copy me",
+            ),
+            title: Text("About", style: TextStyle(fontWeight: FontWeight.w600)),
+            subtitle: Text("About this app......"),
+          ),
+          AboutListTile(),
         ],
       ),
-
-      // body: ListWheelScrollView(
-      //   // useMagnifier: true,
-      //   // magnification: 1.5, // 돋보기 같음
-      //   diameterRatio: 1, // 100일때 곡률이 보이지않음
-      //   offAxisFraction: 2, // 원통이 살짝 옆으로 돌아감
-      //   itemExtent: 200,
-      //   children: [
-      //     for (var x in [1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1])
-      //       FractionallySizedBox(
-      //         widthFactor: 1,
-      //         child: Container(
-      //           color: Colors.teal,
-      //           alignment: Alignment.center,
-      //           child: Text(
-      //             'Pick me',
-      //             style: TextStyle(color: Colors.white, fontSize: 39),
-      //           ),
-      //         ),
-      //       ),
-      //   ],
-      // ),
     );
   }
 }
