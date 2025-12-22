0fa659ffa867143ed0ea6c1b2ea79a6514484fc6
doum <notivior@gmail.com>
2025-12-22 17:41:29 +0900

    feat: #16.5 Pluralize and Select

--- lib/features/authentication/sign_up_screen.dart
+++ lib/features/authentication/sign_up_screen.dart
@@ -54,7 +54,7 @@ class SignUpScreen extends StatelessWidget {
                   Opacity(
                     opacity: 0.7,
                     child: Text(
-                      S.of(context).signUpSubtitle,
+                      S.of(context).signUpSubtitle(1234),
                       style: Theme.of(context).textTheme.titleMedium,
                       textAlign: TextAlign.center,
                     ),
@@ -117,7 +117,7 @@ class SignUpScreen extends StatelessWidget {
                   GestureDetector(
                     onTap: () => _onLoginTap(context),
                     child: Text(
-                      S.of(context).logIn,
+                      S.of(context).logIn("female"),
                       style: TextStyle(
                         fontWeight: FontWeight.w600,
                         color: Theme.of(context).primaryColor,
--- lib/generated/l10n.dart
+++ lib/generated/l10n.dart
@@ -74,13 +74,13 @@ class S {
     );
   }
 
-  /// `Create a profile, follow other accounts, make your own videos, and more.`
-  String get signUpSubtitle {
+  /// `Create a profile, follow other accounts, make your own {videoCount, plural, =0 {no videos} =1 {video} other{videos}}, and more.`
+  String signUpSubtitle(num videoCount) {
     return Intl.message(
-      'Create a profile, follow other accounts, make your own videos, and more.',
+      'Create a profile, follow other accounts, make your own ${Intl.plural(videoCount, zero: 'no videos', one: 'video', other: 'videos')}, and more.',
       name: 'signUpSubtitle',
       desc: '',
-      args: [],
+      args: [videoCount],
     );
   }
 
@@ -114,9 +114,14 @@ class S {
     );
   }
 
-  /// `Log in`
-  String get logIn {
-    return Intl.message('Log in', name: 'logIn', desc: '', args: []);
+  /// `Log in {gender, select, male{sir} female{madam} other{human}}`
+  String logIn(String gender) {
+    return Intl.message(
+      'Log in ${Intl.gender(gender, male: 'sir', female: 'madam', other: 'human')}',
+      name: 'logIn',
+      desc: '',
+      args: [gender],
+    );
   }
 }
 
