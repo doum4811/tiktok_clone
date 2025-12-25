b4957453c3943cc5d35006a0e5e772534f349832
doum <notivior@gmail.com>
2025-12-25 18:02:19 +0900

    feat: #19.1 Installation - #19.2 CameraController

--- lib/router.dart
+++ lib/router.dart
@@ -1,64 +1,12 @@
-import 'dart:math';
-
-import 'package:flutter/widgets.dart';
 import 'package:go_router/go_router.dart';
-import 'package:tiktok_clone/features/authentication/email_screen.dart';
-import 'package:tiktok_clone/features/authentication/login_screen.dart';
-import 'package:tiktok_clone/features/authentication/sign_up_screen.dart';
-import 'package:tiktok_clone/features/authentication/username_screen.dart';
-import 'package:tiktok_clone/features/users/user_profile_screen.dart';
+import 'package:tiktok_clone/features/videos/video_recording_screen.dart';
 
 final router = GoRouter(
   // routes: [GoRoute(path: "/", builder: (context, state) => SignUpScreen())],
   routes: [
     GoRoute(
-      name: SignUpScreen.routeName,
-      path: SignUpScreen.routeURL,
-      builder: (context, state) => SignUpScreen(),
-      routes: [
-        GoRoute(
-          path: UsernameScreen.routeURL,
-          name: UsernameScreen.routeName,
-          builder: (context, state) => UsernameScreen(),
-          routes: [
-            GoRoute(
-              name: EmailScreen.routeName,
-              path: EmailScreen.routeURL,
-              builder: (context, state) {
-                final agrs = state.extra as EmailScreenArgs;
-                return EmailScreen(username: agrs.username);
-              },
-            ),
-          ],
-        ),
-      ],
-    ),
-
-    // GoRoute(path: LoginScreen.routeName, builder: (context, state) => LoginScreen()),
-    /*     
-    GoRoute(
-      name: "username_screen",
-      path: UsernameScreen.routeName,
-      pageBuilder: (context, state) {
-        return CustomTransitionPage(
-          child: UsernameScreen(),
-          transitionsBuilder: (context, animation, secondaryAnimation, child) {
-            return FadeTransition(
-              opacity: animation,
-              child: ScaleTransition(scale: animation, child: child),
-            );
-          },
-        );
-      },
-    
-    ), */
-    GoRoute(
-      path: "/users/:username",
-      builder: (context, state) {
-        final username = state.params["username"];
-        final tab = state.queryParams["show"];
-        return UserProfileScreen(username: username!, tab: tab!);
-      },
+      path: "/",
+      builder: (context, state) => const VideoRecordingScreen(),
     ),
   ],
 );
