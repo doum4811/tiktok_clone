e6ac203ec584265b4c3ad0dd95907cca4c652493
doum <notivior@gmail.com>
2025-12-31 17:28:00 +0900

    feat: #20.11 Provider

--- lib/features/videos/widgets/video_post.dart
+++ lib/features/videos/widgets/video_post.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:provider/provider.dart';
 import 'package:tiktok_clone/common/widgets/main_navigation/video_config/video_config.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
@@ -34,7 +35,6 @@ class _VideoPostState extends State<VideoPost>
   late final AnimationController _animationController;
 
   bool _isPaused = false;
-  bool _autoMute = videoConfig.value;
 
   final Duration _animationDuration = Duration(milliseconds: 200);
 
@@ -71,12 +71,6 @@ class _VideoPostState extends State<VideoPost>
       value: 1.5,
       duration: _animationDuration,
     );
-
-    videoConfig.addListener(() {
-      setState(() {
-        _autoMute = videoConfig.value;
-      });
-    });
   }
 
   @override
@@ -172,13 +166,13 @@ class _VideoPostState extends State<VideoPost>
             top: 40,
             child: IconButton(
               icon: FaIcon(
-                _autoMute
+                context.watch<VideoConfig>().isMuted
                     ? FontAwesomeIcons.volumeOff
                     : FontAwesomeIcons.volumeHigh,
                 color: Colors.white,
               ),
               onPressed: () {
-                videoConfig.value = !videoConfig.value;
+                context.read<VideoConfig>().toggleIsMuted();
               },
             ),
           ),
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 import 'package:tiktok_clone/common/widgets/main_navigation/video_config/video_config.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/authentication/email_screen.dart';
@@ -32,82 +33,87 @@ class TikTokApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // S.load(const Locale('en'));
-    return MaterialApp.router(
-      routerConfig: router,
-      debugShowCheckedModeBanner: false,
-      title: 'TikTok Clone',
-      localizationsDelegates: [
-        S.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-      ],
-      supportedLocales: [Locale('en'), Locale('ko')],
-      themeMode: ThemeMode.system, //  light,
-      theme: ThemeData(
-        // useMaterial3: false,
-        useMaterial3: true,
+    // return ChangeNotifierProvider(
+    //   create: (context) => VideoConfig(),
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (context) => VideoConfig())],
+      child: MaterialApp.router(
+        routerConfig: router,
+        debugShowCheckedModeBanner: false,
+        title: 'TikTok Clone',
+        localizationsDelegates: [
+          S.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+        ],
+        supportedLocales: [Locale('en'), Locale('ko')],
+        themeMode: ThemeMode.system, //  light,
+        theme: ThemeData(
+          // useMaterial3: false,
+          useMaterial3: true,
 
-        brightness: Brightness.light,
-        // textTheme: Typography.blackCupertino,
-        textTheme: Typography.blackMountainView,
+          brightness: Brightness.light,
+          // textTheme: Typography.blackCupertino,
+          textTheme: Typography.blackMountainView,
 
-        scaffoldBackgroundColor: Colors.white,
-        bottomAppBarTheme: BottomAppBarThemeData(color: Colors.grey.shade50),
-        primaryColor: Color(0xFFE9435A),
-        textSelectionTheme: TextSelectionThemeData(
-          cursorColor: Color(0xFFE9435A),
-        ),
-        splashColor: Colors.transparent,
-        // highlightColor: Colors.transparent,
-        appBarTheme: AppBarTheme(
-          foregroundColor: Colors.black,
-          backgroundColor: Colors.white,
-          surfaceTintColor: Colors.white,
-          elevation: 0,
-          titleTextStyle: TextStyle(
-            color: Colors.black,
-            fontSize: Sizes.size16 + Sizes.size2,
-            fontWeight: FontWeight.w600,
+          scaffoldBackgroundColor: Colors.white,
+          bottomAppBarTheme: BottomAppBarThemeData(color: Colors.grey.shade50),
+          primaryColor: Color(0xFFE9435A),
+          textSelectionTheme: TextSelectionThemeData(
+            cursorColor: Color(0xFFE9435A),
+          ),
+          splashColor: Colors.transparent,
+          // highlightColor: Colors.transparent,
+          appBarTheme: AppBarTheme(
+            foregroundColor: Colors.black,
+            backgroundColor: Colors.white,
+            surfaceTintColor: Colors.white,
+            elevation: 0,
+            titleTextStyle: TextStyle(
+              color: Colors.black,
+              fontSize: Sizes.size16 + Sizes.size2,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          tabBarTheme: TabBarThemeData(
+            //TabBarTheme
+            labelColor: Colors.black,
+            unselectedLabelColor: Colors.grey.shade500,
+            indicatorColor: Colors.black,
           ),
         ),
-        tabBarTheme: TabBarThemeData(
-          //TabBarTheme
-          labelColor: Colors.black,
-          unselectedLabelColor: Colors.grey.shade500,
-          indicatorColor: Colors.black,
-        ),
-      ),
-      darkTheme: ThemeData(
-        // useMaterial3: false,
-        useMaterial3: true,
-        tabBarTheme: TabBarThemeData(
-          //TabBarTheme
-          labelColor: Colors.white,
-          unselectedLabelColor: Colors.grey.shade700,
-          indicatorColor: Colors.white,
-        ),
-        brightness: Brightness.dark,
-        textSelectionTheme: TextSelectionThemeData(
-          cursorColor: Color(0xFFE9435A),
-        ),
-        textTheme: Typography.whiteMountainView,
-
-        scaffoldBackgroundColor: Colors.black,
-        appBarTheme: AppBarTheme(
-          surfaceTintColor: Colors.grey.shade900,
-          backgroundColor: Colors.grey.shade900,
-          foregroundColor: Colors.white,
-          titleTextStyle: TextStyle(
-            color: Colors.white,
-            fontSize: Sizes.size16 + Sizes.size2,
-            fontWeight: FontWeight.w600,
+        darkTheme: ThemeData(
+          // useMaterial3: false,
+          useMaterial3: true,
+          tabBarTheme: TabBarThemeData(
+            //TabBarTheme
+            labelColor: Colors.white,
+            unselectedLabelColor: Colors.grey.shade700,
+            indicatorColor: Colors.white,
           ),
-          actionsIconTheme: IconThemeData(color: Colors.grey.shade100),
-        ),
-        bottomAppBarTheme: BottomAppBarThemeData(color: Colors.grey.shade900),
+          brightness: Brightness.dark,
+          textSelectionTheme: TextSelectionThemeData(
+            cursorColor: Color(0xFFE9435A),
+          ),
+          textTheme: Typography.whiteMountainView,
 
-        primaryColor: Color(0xFFE9435A),
+          scaffoldBackgroundColor: Colors.black,
+          appBarTheme: AppBarTheme(
+            surfaceTintColor: Colors.grey.shade900,
+            backgroundColor: Colors.grey.shade900,
+            foregroundColor: Colors.white,
+            titleTextStyle: TextStyle(
+              color: Colors.white,
+              fontSize: Sizes.size16 + Sizes.size2,
+              fontWeight: FontWeight.w600,
+            ),
+            actionsIconTheme: IconThemeData(color: Colors.grey.shade100),
+          ),
+          bottomAppBarTheme: BottomAppBarThemeData(color: Colors.grey.shade900),
+
+          primaryColor: Color(0xFFE9435A),
+        ),
       ),
     );
   }
